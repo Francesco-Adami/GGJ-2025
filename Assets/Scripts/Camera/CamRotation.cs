85be268b83eb745a2a8d0f33318aa91d408cf108
Francesco Adami <francescoadami9@gmail.com>
2025-01-25 23:16:09 +0100

    cinemachine

--- Assets/Scripts/Camera/CamRotation.cs
+++ Assets/Scripts/Camera/CamRotation.cs
@@ -11,17 +11,25 @@ public class PlayerMovement : MonoBehaviour
     private float xRotation;
     private float yRotation;
 
+    [Header("Transform")]
     public Transform point;
+    public Transform gun;
     public Transform player;
+    public Transform head;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.position = point.position;
         transform.rotation = point.rotation;
 
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * xSense;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * ySense;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.fixedDeltaTime * xSense;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.fixedDeltaTime * ySense;
 
         yRotation += mouseY;
         xRotation += mouseX;
@@ -30,7 +38,11 @@ public class PlayerMovement : MonoBehaviour
         //xRotation = Mathf.Clamp(xRotation,-90,90);
         yRotation = Mathf.Clamp(yRotation, -90, 90);
 
-        player.rotation = Quaternion.Euler(0, xRotation, 0);
-        point.rotation = Quaternion.Euler(-yRotation, xRotation, 0);
+        //player.rotation = Quaternion.Euler(0, xRotation, 0);
+        player.Rotate(new Vector3(0, mouseX, 0));
+        point.Rotate(new Vector3(-mouseY, 0, 0));
+        gun.Rotate(new Vector3(0, 0, -mouseY));
+        head.Rotate(new Vector3(0, 0, mouseY));
+        //gun.rotation = Quaternion.Euler(-yRotation, xRotation, 0);
     }
 }
